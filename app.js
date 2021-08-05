cce1a7d1b013960d625a59158ac7567a077a25b5
elvinakurhanska <elvinakurhanska@github.com>
2021-08-04 23:30:12 -0400

    needRefuct

--- app.js
+++ app.js
@@ -1,6 +1,88 @@
 
 const express = require('express')
 const cors = require('cors')
+const {MongoClient} = require('mongodb');
+
+const dbName = "hello-lolik";
+
+async function listDatabases(client) {
+    databasesList = await client
+        .db()
+        .admin()
+        .listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+    // databasesList.
+}
+
+async function main(site, password) {
+    const uri = "mongodb+srv://Elle:jk@cluster0.vxve0.mongodb.net/ProjectPassword?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    try {
+        // await client.connect();
+        // await listDatabases(client);
+
+        // var dbo = db.db("mydb");
+        // var myobj = { name: "Company Inc", address: "Highway 37" };
+        // dbo.collection("customers").insertOne(myobj, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document inserted");
+        //     db.close();
+        // });
+
+
+        await client.connect();
+
+         console.log("Connected correctly to server");
+
+         const db = client.db(dbName);
+
+         // Use the collection "people"
+
+         const col = db.collection("passwords");
+
+         // Construct a document                                                                                                                                                              
+
+         let personDocument = {
+
+             "name": { "first": "Alan", "last": "Turing" },
+
+             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+
+             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+
+             "views": 1250000
+
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+
+         const obj = {
+             site,
+             password
+         }
+
+         const p = await col.insertOne(obj);
+
+         // Find one document
+
+         const myDoc = await col.findOne();
+
+         // Print to the console
+
+         console.log(myDoc);
+        
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
 
 const app = express()
 app.use(cors())
@@ -25,6 +107,12 @@ app.post('/create-password', function (req, res) {
     }
 
     passwords[site] = password
+
+
+    // test
+    main(site, password)
+    // end test
+
     res.sendStatus(200)
 })
 
@@ -53,4 +141,5 @@ app.delete('/delete-password', (req, res) => {
     res.sendStatus(200)
 })
 
+// main().catch(console.error);
 app.listen(3000)
